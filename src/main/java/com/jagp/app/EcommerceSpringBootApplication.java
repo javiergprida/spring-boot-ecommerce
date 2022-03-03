8d5fc7673ac8a3e6d73367764dacd4b4255a8034
JavierGPrida <javiierpriida@gmail.com>
2022-03-02 21:31:04 -0300

    creacion de proyecto y estructuracion de carpetas y las clases modelo

---
+++ src/main/java/com/jagp/app/EcommerceSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.jagp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class EcommerceSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jagp/app/EcommerceSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jagp.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
