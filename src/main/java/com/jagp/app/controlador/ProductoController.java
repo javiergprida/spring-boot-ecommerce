47d4f4a6b43f778fb3b803e295a52212506ab8d4
JavierGPrida <javiierpriida@gmail.com>
2022-03-08 20:37:49 -0300

    creacion funccion save producto y logger de clase controller

--- src/main/java/com/jagp/app/controlador/ProductoController.java
+++ src/main/java/com/jagp/app/controlador/ProductoController.java
@@ -1,15 +1,44 @@
 package com.jagp.app.controlador;
 
+
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jagp.app.modelo.Producto;
+import com.jagp.app.modelo.Usuario;
+import com.jagp.app.servicio.ProductoServices;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
 
+	private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
+	
+	@Autowired
+	private ProductoServices productoServicio;
+	
 	@GetMapping("")
 	public String show() {
 		return "productos/show";
 	}
+	
+	@GetMapping("/create")
+	public String create() {
+		return "productos/create";
+	}
+	
+	@PostMapping("/save")
+	public String saveProducto(Producto producto) {
+		LOGGER.info("este es el objeto producto {}",producto);
+		Usuario user = new Usuario(1, "", "", "", "", "", "", null);
+		producto.setUsuario(user);
+		productoServicio.saveProducto(producto);
+		return "redirect:/productos";
+	}
+	
+	
 }
