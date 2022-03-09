113c1be7ffabe498b57137dbb5004244e86c9fd5
JavierGPrida <javiierpriida@gmail.com>
2022-03-09 19:06:43 -0300

    mostrar la vista del producto al usuario

--- src/main/java/com/jagp/app/controlador/HomeController.java
+++ src/main/java/com/jagp/app/controlador/HomeController.java
@@ -2,18 +2,22 @@ package com.jagp.app.controlador;
 
 import java.util.List;
 
+import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.jagp.app.modelo.Producto;
 import com.jagp.app.servicio.ProductoServices;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping(value={"", "/", "/home"})
 public class HomeController {
+	
+	private final Logger log = LoggerFactory.getLogger(HomeController.class);
 	
 	@Autowired
 	private ProductoServices poductoServicio;
@@ -25,6 +29,12 @@ public class HomeController {
 		model.addAttribute("productos", productos);
 		
 		return "usuario/home";
+	}
+	
+	@GetMapping("producto_home/{id}")
+	public String productoHome(@PathVariable Integer id) {
+		log.info("id enviado como parametro {}", id);
+		return "usuario/producto_home";
 	}
 
 }
