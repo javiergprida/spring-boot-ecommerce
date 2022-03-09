3dd0895ed92acec27de327be9a7ed7afec16c090
JavierGPrida <javiierpriida@gmail.com>
2022-03-09 20:09:20 -0300

    redireccionarmos al carrito de compras

--- src/main/java/com/jagp/app/controlador/HomeController.java
+++ src/main/java/com/jagp/app/controlador/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.jagp.app.modelo.Producto;
@@ -43,5 +44,10 @@ public class HomeController {
 		
 		return "usuario/producto_home";
 	}
+	
+	@PostMapping("/carrito")
+	public String addCarrito() {
+		return "usuario/carrito";
+	} 
 
 }
