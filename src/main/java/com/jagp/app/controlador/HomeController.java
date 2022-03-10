55081473014dedc08053d3606badff28f7a2fcdc
JavierGPrida <javiierpriida@gmail.com>
2022-03-09 21:47:46 -0300

    validacion doble producto y ver carrito desde menu

--- src/main/java/com/jagp/app/controlador/HomeController.java
+++ src/main/java/com/jagp/app/controlador/HomeController.java
@@ -75,7 +75,15 @@ public class HomeController {
 		detalleOrden.setTotal(producto.getPrecio() * cantidad);
 		detalleOrden.setProducto(producto);
 		
-		detalles.add(detalleOrden);
+		//validar que el producto no se añada repetidas veces
+		Integer idProducto = producto.getId();
+		boolean ingresado = detalles.stream().anyMatch(p -> p.getProducto().getId() == idProducto);
+		if(!ingresado) {
+			detalles.add(detalleOrden);
+		}
+		
+		
+		
 		
 		sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
 		
@@ -120,4 +128,13 @@ public class HomeController {
 		return "usuario/carrito";
 	}
 
+	@GetMapping("/getCarrito")
+	public String getCarrito(Model model) {
+		
+		model.addAttribute("cart", detalles);
+		model.addAttribute("orden", orden);
+		
+		return "/usuario/carrito";
+	}
+	
 }
