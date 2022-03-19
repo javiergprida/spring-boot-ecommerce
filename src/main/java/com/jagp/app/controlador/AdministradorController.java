77c4a8c69218d2344683cedfa93e1c89fad779d6
JavierGPrida <javiierpriida@gmail.com>
2022-03-19 11:13:15 -0300

    funcionalidad para ver las ordenes por admin

--- src/main/java/com/jagp/app/controlador/AdministradorController.java
+++ src/main/java/com/jagp/app/controlador/AdministradorController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.jagp.app.modelo.Usuario;
+import com.jagp.app.servicio.IOrdenServices;
 import com.jagp.app.servicio.IProductoServices;
 import com.jagp.app.servicio.IUsuarioServices;
 
@@ -23,6 +24,9 @@ public class AdministradorController {
 	
 	@Autowired
 	private IUsuarioServices usuarioServicio;
+	
+	@Autowired
+	private IOrdenServices ordenServicio;
 	
 	@GetMapping("")
 	public String home(Model model, HttpSession session) {
@@ -47,12 +51,22 @@ public class AdministradorController {
 	@GetMapping("/usuarios")
 	public String verUsuario(HttpSession session, Model model) {
      Usuario usuario = usuarioServicio.findUsuarioById(Integer.parseInt(session.getAttribute("id_usuario").toString())).get();
-     
 		
 		model.addAttribute("usuario", usuario);
+		
 		model.addAttribute("perfiles", usuarioServicio.findAllUser());
 		
 		return "administrador/usuarios";
+	}
+	
+	@GetMapping("/ordenes")
+	public String verOrdenes(HttpSession session, Model model) {
+		 Usuario usuario = usuarioServicio.findUsuarioById(Integer.parseInt(session.getAttribute("id_usuario").toString())).get();
+			
+			model.addAttribute("usuario", usuario);
+			model.addAttribute("ordenes", ordenServicio.findAllOrden());
+		
+		return "administrador/ordenes";
 	}
 
 }
